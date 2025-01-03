b5551dfc042d323cedc3a33b67f2b886567d6d2f
Tamara Mitrovska <tamaram@mit.edu>
2025-01-04 00:37:39 +0100

    add man

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WordComponent } from './word/word.component';
+import { ManComponent } from './man/man.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WordComponent,
+    ManComponent
   ],
   imports: [
     BrowserModule
